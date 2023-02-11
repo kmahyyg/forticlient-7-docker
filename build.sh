b8823d687b4a99ff890286590053a09cd4f59f5a
Patrick Young <16604643+kmahyyg@users.noreply.github.com>
2023-02-11 23:58:00 +0800

    fix typo

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git describe --long --dirty --tags --always | tr -d '\n' > ./gosrc/cmd/version.txt
+git describe --long --dirty --tags --always | tr -d '\n' > ./gosrc/answerBot/version.txt
 sudo podman build . -t ghcr.io/kmahyyg/fortivpn:7 -f ./Forticlient.dockerfile
